6062ee20b3ef181c9556935c0cd591cda58e8fea
Johan Gonzalez <johanfer_12@hotmail.com>
2023-10-08 14:23:08 -0500

    Data cleaning, and temp unit conversion

--- app.R
+++ app.R
@@ -14,7 +14,7 @@ ui <- fluidPage(
     sidebarPanel(
       selectInput("ciudad", "Selecciona una ciudad:",
         choices = unique(data$City),
-        selected = "CiudadInicial"
+        selected = "Bogota"
       )
     ),
     mainPanel(
@@ -22,7 +22,6 @@ ui <- fluidPage(
     )
   )
 )
-
 
 server <- function(input, output) {
   # Filtrar los datos según la ciudad seleccionada
@@ -34,17 +33,39 @@ server <- function(input, output) {
     }
   })
 
-  # Crear la gráfica de línea
+  # Crear la gráfica de línea con línea de tendencia
   output$grafica <- renderPlotly({
     if (!is.null(datos_ciudad())) {
-      p <- ggplot(datos_ciudad(), aes(x = as.Date(paste(Year, Month, Day, sep = "-")), y = AvgTemperature)) +
+      datos_filtrados <- datos_ciudad() %>%
+        filter(AvgTemperature != -99)  # Filtra los valores de temperatura igual a -99
+
+      # Función para convertir Fahrenheit a Celsius
+      convertir_a_celsius <- function(fahrenheit) {
+        celsius <- (fahrenheit - 32) * (5/9)
+        return(celsius)
+      }
+
+      # Aplicar la conversión a la columna AvgTemperature
+      datos_filtrados$AvgTemperature <- convertir_a_celsius(datos_filtrados$AvgTemperature)
+
+      # Agrupar los datos por mes y calcular la temperatura promedio
+      datos_agrupados <- datos_filtrados %>%
+        mutate(Fecha = as.Date(paste(Year, Month, "01", sep = "-"))) %>%
+        group_by(Fecha) %>%
+        summarize(TempPromedio = mean(AvgTemperature))
+
+      p <- ggplot(datos_agrupados, aes(x = Fecha, y = TempPromedio)) +
         geom_line() +
-        labs(x = "Fecha", y = "Temperatura Promedio") +
-        theme_minimal()
+        geom_smooth(method = "lm", se = FALSE, color = "blue") +  # Agrega la línea de tendencia
+        labs(x = "Fecha", y = "Temperatura Promedio (°C)") +
+        scale_x_date(date_labels = "%Y", date_breaks = "1 year") +  # Personaliza la escala del eje x
+        theme_minimal() +
+        theme(axis.text.x = element_text(angle = 90, hjust = 1))  # Orientación vertical de las etiquetas
 
       ggplotly(p, tooltip = "y")  # Convierte la gráfica de ggplot a plotly
     }
   })
+
 }
 
 # Ejecutar la aplicación Shiny
